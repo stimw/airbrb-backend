3dd7e04536da594a91d27511c657bc58f5469956
stimw <zhiwai98@gmail.com>
2023-01-24 17:16:35 +1100

    update port

--- src/server.js
+++ src/server.js
@@ -259,8 +259,9 @@ app.get('/', (req, res) => res.redirect('/docs'));
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const configData = JSON.parse(fs.readFileSync('../frontend/src/config.json'));
-const port = 'BACKEND_PORT' in configData ? configData.BACKEND_PORT : 5033;
+// const configData = JSON.parse(fs.readFileSync('../frontend/src/config.json'));
+// const port = 'BACKEND_PORT' in configData ? configData.BACKEND_PORT : 5033;
+const port = process.env.PORT || 8080;
 
 const server = app.listen(port, () => {
   console.log(`Backend is now listening on port ${port}!`);
